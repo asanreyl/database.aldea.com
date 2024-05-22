7c23f5577f3262a905788d58a34664fb15c80de0
Angel Luis Santana Reyes <angel@donicus.com>
2024-05-22 20:09:40 +0100

    recogidad datos semanales ok

--- procedures/wattermeter/getWatterMeterReadingList.sql
+++ procedures/wattermeter/getWatterMeterReadingList.sql
@@ -23,7 +23,7 @@ BEGIN
 
     -- Crear una tabla temporal con todas las fechas únicas de lecturas para cada contador
     CREATE TEMPORARY TABLE fechas_unicas AS (
-        SELECT idContador, DATE(fecha) AS fecha_dia, 
+        SELECT idContador, DATE(fecha) AS fecha_dia, TIME(fecha) AS hora,
             CASE
                 WHEN TIME(fecha) < '14:00:00' THEN 'M'
                 ELSE 'T'
@@ -31,7 +31,7 @@ BEGIN
         FROM lecturascl
         WHERE DATE(fecha) BETWEEN _fechaLunes AND _fechaDomingo
         UNION
-        SELECT idContador, DATE(fecha) AS fecha_dia, 
+        SELECT idContador, DATE(fecha) AS fecha_dia, TIME(fecha) AS hora,
             CASE
                 WHEN TIME(fecha) < '14:00:00' THEN 'M'
                 ELSE 'T'
@@ -39,7 +39,7 @@ BEGIN
         FROM lecturasph
         WHERE DATE(fecha) BETWEEN _fechaLunes AND _fechaDomingo
         UNION
-        SELECT idContador, DATE(fecha) AS fecha_dia, 
+        SELECT idContador, DATE(fecha) AS fecha_dia, TIME(fecha) AS hora,
             CASE
                 WHEN TIME(fecha) < '14:00:00' THEN 'M'
                 ELSE 'T'
@@ -50,29 +50,29 @@ BEGIN
 
     -- Combinamos las fechas únicas con cada tabla de lecturas
     CREATE TEMPORARY TABLE lecturas_combinadas AS (
-        SELECT f.idcontador, f.fecha_dia, f.periodo, 
+        SELECT f.idcontador, f.fecha_dia, f.hora, f.periodo, 
         	c.valorCloro AS valor_cl, 
         	p.valorPh AS valor_ph, 
         	v.valorMCubicos AS valor_volumen
         FROM fechas_unicas f
-        LEFT JOIN (SELECT idContador, DATE(fecha) AS fecha_dia, 
+        LEFT JOIN (SELECT idContador, DATE(fecha) AS fecha_dia, TIME(fecha) AS hora,
             CASE
                 WHEN TIME(fecha) < '14:00:00' THEN 'M'
                 ELSE 'T'
             END AS periodo, valorCloro FROM lecturascl) c 
-            ON f.idContador = c.idContador AND f.fecha_dia = c.fecha_dia AND f.periodo = c.periodo
-        LEFT JOIN (SELECT idContador, DATE(fecha) AS fecha_dia, 
+            ON f.idContador = c.idContador AND f.fecha_dia = c.fecha_dia AND f.hora = c.hora AND f.periodo = c.periodo
+        LEFT JOIN (SELECT idContador, DATE(fecha) AS fecha_dia, TIME(fecha) AS hora,
             CASE
                 WHEN TIME(fecha) < '14:00:00' THEN 'M'
                 ELSE 'T'
             END AS periodo, valorPh FROM lecturasph) p 
-            ON f.idContador = p.idContador AND f.fecha_dia = p.fecha_dia AND f.periodo = p.periodo
-        LEFT JOIN (SELECT idContador, DATE(fecha) AS fecha_dia, 
+            ON f.idContador = p.idContador AND f.fecha_dia = p.fecha_dia AND f.hora = p.hora AND f.periodo = p.periodo
+        LEFT JOIN (SELECT idContador, DATE(fecha) AS fecha_dia, TIME(fecha) AS hora,
             CASE
                 WHEN TIME(fecha) < '14:00:00' THEN 'M'
                 ELSE 'T'
             END AS periodo, valorMCubicos FROM lecturasmetroscubicos) v 
-            ON f.idContador = v.idContador AND f.fecha_dia = v.fecha_dia AND f.periodo = v.periodo
+            ON f.idContador = v.idContador AND f.fecha_dia = v.fecha_dia AND f.hora = v.hora AND f.periodo = v.periodo
     );
 
 SELECT JSON_ARRAYAGG(
@@ -80,10 +80,11 @@ SELECT JSON_ARRAYAGG(
             'idContador', encrypt(lc.idcontador),
             'nombre', co.nombre,
             'fecha', lc.fecha_dia,
+            'hora', lc.hora,
             'periodo', lc.periodo,
-            'cl', lc.valor_cl,
-            'ph', lc.valor_ph,
-            'volumen', lc.valor_volumen
+            'cl', round(lc.valor_cl,2),
+            'ph', round(lc.valor_ph,2),
+            'volumen', round(lc.valor_volumen,2)
         )
     ) AS data_json
     FROM lecturas_combinadas lc
